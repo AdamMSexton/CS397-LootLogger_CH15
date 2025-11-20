ee463314faf2e756c07ef2b210010f98dca0411d
Adam Sexton <asexton1@jacksonville.edu>
2025-11-20 13:22:37 -0500

    Initial Commit

---
+++ LootLogger/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  LootLogger
+//
+//  Created by Apple Sucks on 10/29/25.
+//
+
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
---
+++ LootLoggerTests/LootLoggerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LootLoggerTests.swift
+//  LootLoggerTests
+//
+//  Created by Apple Sucks on 10/29/25.
+//
+
+import Testing
+@testable import LootLogger
+
+struct LootLoggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
